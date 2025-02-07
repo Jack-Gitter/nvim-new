af9931b387a409305bf01e87f059c5e6cd308086
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-07 14:30:50 -0500

    nice

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,2 +1,8 @@
-return {"lewis6991/gitsigns.nvim", opts = {}}
+local on_attach = function()
+	local gitsigns = require("gitsigns")
+	vim.keymap.set("n", "<leader>gr", gitsigns.reset_hunk, {buffer = 0})
+	vim.keymap.set("n", "<leader>gb", gitsigns.blame, {buffer = 0})
+end
+
+return {"lewis6991/gitsigns.nvim", opts = {on_attach = on_attach}}
 
