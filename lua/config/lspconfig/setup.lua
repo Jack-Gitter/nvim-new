9110755cc2296ec12d324698223f2b8818927502
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-07 15:55:36 -0500

    nice

--- lua/config/lspconfig/setup.lua
+++ lua/config/lspconfig/setup.lua
@@ -2,15 +2,16 @@ local lspconfig = require("lspconfig")
 local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 local on_attach = function()
-	vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {buffer = 0})
+	vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, { buffer = 0 })
 end
 
 lspconfig.lua_ls.setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
 	settings = {
-		Lua = {diagnostics = { globals = { 'vim' }}}
+		Lua = { diagnostics = { globals = { 'vim' } } }
 	}
 })
 
-lspconfig.ts_ls.setup({capabilities = capabilities, on_attach = on_attach})
+lspconfig.ts_ls.setup({ capabilities = capabilities, on_attach = on_attach })
+lspconfig.eslint.setup({ capabilities = capabilities, on_attach = on_attach })
