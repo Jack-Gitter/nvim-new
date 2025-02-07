0130cc8a2c42bc575c51e75dd0ea9c753bf97a80
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-07 11:29:32 -0500

    nice

--- lua/config/lspconfig/setup.lua
+++ lua/config/lspconfig/setup.lua
@@ -1,9 +1,11 @@
 local lspconfig = require("lspconfig")
+local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 lspconfig.lua_ls.setup({
+	capabilities = capabilities,
 	settings = {
 		Lua = {diagnostics = { globals = { 'vim' }}}
 	}
 })
 
-lspconfig.ts_ls.setup({})
+lspconfig.ts_ls.setup({capabilities = capabilities})
