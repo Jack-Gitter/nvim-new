0509c524f4190cba420e6038a89c669b114a0d2b
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-07 11:38:35 -0500

    nice

--- lua/plugins/blinkcmp.lua
+++ lua/plugins/blinkcmp.lua
@@ -3,6 +3,11 @@ return {
 	dependencies = 'rafamadriz/friendly-snippets',
 	version = '*',
 	opts = {
-		keymap = { preset = 'default' },
+		keymap = {
+			preset = 'default',
+			["<C-j>"] = {'select_next', 'fallback'},
+			["<C-k>"] = {'select_prev', 'fallback'},
+			["<C-CR>"] = {'select_and_accept', 'fallback'}
+		},
 	},
 }
