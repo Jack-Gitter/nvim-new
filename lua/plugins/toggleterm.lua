82c4a5c09abeffe4fd74e88c9ebc47bbff3b5187
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-10 11:20:31 -0500

    got floatterm working

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		'akinsho/toggleterm.nvim',
+		version = "*",
+		opts = {
+			direction = 'float',
+			open_mapping = "<leader>tt",
+			float_opts = {
+				border = 'curved',
+				title_pos = 'center'
+			}
+		}
+	}
+}
