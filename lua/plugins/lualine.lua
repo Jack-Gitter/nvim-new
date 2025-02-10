4cf65e3fbe0e6e119798ccfa1da2c15b7cb66863
Jack Gitter <jack.gitter@gmail.com>
2025-02-10 18:13:58 -0500

    added lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+	'nvim-lualine/lualine.nvim',
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	opts = {
+		sections = {
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = { 'location' }
+		}
+	},
+}
