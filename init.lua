a758d4c6a599d48a1ba3f4e7af70e54784d688f2
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-11 12:07:04 -0500

    ok

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require("config.lazy.setup")
 
 --------  General --------
 require("config.general.settings")
+require("config.general.keymaps")
 
 -------- Diagnostics --------
 require("config.diagnostics.setup")
