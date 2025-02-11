a38850b04791a58784b27f3f3a7a0cccb9bd7f7a
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-11 15:38:57 -0500

    meh

--- lua/plugins/nvmcmp.lua
+++ lua/plugins/nvmcmp.lua
@@ -3,13 +3,13 @@ return {
 	event = { "BufReadPost", "BufNewFile" },
 	dependencies = {
 		{
-			"hrsh7th/cmp-buffer"
-		},
-		{
 			"hrsh7th/cmp-nvim-lsp",
 		},
 		{
 			"L3MON4D3/LuaSnip",
+		},
+		{
+			"hrsh7th/cmp-buffer"
 		},
 	},
 	opts = {
