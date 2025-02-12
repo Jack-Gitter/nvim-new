bf6ec4806198795d19db2a10f18ebd1d70b7ab8e
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-12 09:05:51 -0500

    adjusted keybindings

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,7 +2,7 @@ return {
 	"ggandor/leap.nvim",
 	dependencies = { "tpope/vim-repeat" },
 	config = function()
-		vim.keymap.set({ 'n', 'x', 'o' }, '<leader>s', '<Plug>(leap-forward)')
-		vim.keymap.set({ 'n', 'x', 'o' }, '<leader>S', '<Plug>(leap-backward)')
+		vim.keymap.set('n', 'f', '<Plug>(leap-forward)')
+		vim.keymap.set('n', 'F', '<Plug>(leap-backward)')
 	end
 }
