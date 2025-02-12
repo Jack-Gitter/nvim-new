8a10d618a0531c4ddc0bda025024e901af2bd3f4
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-12 15:25:09 -0500

    sure

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,5 +1,6 @@
 -- return {"navarasu/onedark.nvim", opts = {}, lazy = false}
 -- return { 'ribru17/bamboo.nvim', config = function() require('bamboo').setup({}) require('bamboo').load() end, lazy = false }
-return { "ellisonleao/gruvbox.nvim", config = function() vim.cmd([[colorscheme gruvbox]]) end, opts = {}, lazy = false }
+-- return { "ellisonleao/gruvbox.nvim", config = function() vim.cmd([[colorscheme gruvbox]]) end, opts = {}, lazy = false }
 -- return { "Mofiqul/dracula.nvim", config = function() vim.cmd([[colorscheme dracula]]) end, opts = {}, lazy = false }
 -- return { "rose-pine/neovim", name = "rose-pine", config = function() vim.cmd("colorscheme rose-pine") end, lazy = false }
+return {}
