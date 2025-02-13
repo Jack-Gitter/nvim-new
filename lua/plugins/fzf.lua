6f5f6f2a5dc43d4ec7ad3536f6820f0a69f6725c
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-13 10:49:11 -0500

    testing

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -10,7 +10,7 @@ return {
 	config = function(plugin, opts)
 		require("fzf-lua").setup(opts)
 		vim.keymap.set("n", "<leader>ff", "<Cmd>FzfLua files<CR>")
-		vim.keymap.set("n", "<leader>b", "<Cmd>FzfLua buffers<CR>")
+		vim.keymap.set("n", "<leader>bu", "<Cmd>FzfLua buffers<CR>")
 		vim.keymap.set("n", "<leader>gr", "<Cmd>FzfLua live_grep<CR>")
 		vim.keymap.set("n", "<leader>gd", "<Cmd>FzfLua lsp_definitions<CR>")
 		vim.keymap.set("n", "<leader>fr", "<Cmd>FzfLua lsp_references<CR>")
