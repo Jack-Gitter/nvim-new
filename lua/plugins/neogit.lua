d49610c9472abf66e6eec41923015405df24ed11
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-13 10:10:36 -0500

    thank god, its now working!

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -11,9 +11,8 @@ return {
 			show_staged_diff = false,
 		}
 	},
-	config = function(opts)
+	config = function(_, opts)
 		local neogit = require("neogit")
-		print(opts.kind)
 		neogit.setup(opts)
 		vim.keymap.set("n", "<leader>git", function() neogit.open({ kind = "floating" }) end)
 	end
