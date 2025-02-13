87f57034b15be0b751b9dbdcf14f677ec9177088
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-13 09:47:47 -0500

    ok

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -9,11 +9,5 @@ return {
 		local neogit = require("neogit")
 		neogit.setup()
 		vim.keymap.set("n", "<leader>git", function() neogit.open({ kind = "floating" }) end)
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "NeogitPushComplete",
-			callback = function()
-				print("done!")
-			end
-		})
 	end
 }
