b06824bb4addeeebb21c0cd9661ca771dcf797f0
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-14 15:31:40 -0500

    nice

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,6 +1,7 @@
--- return { "navarasu/onedark.nvim", opts = {}, lazy = false }
+return { "navarasu/onedark.nvim", opts = {}, config = function() require("onedark").load() end, lazy = false }
+-- return { "Mofiqul/dracula.nvim", config = function() vim.cmd("colorscheme dracula") end, opts = {}, lazy = false }
+-- return { "rose-pine/neovim", name = "rose-pine", config = function() vim.cmd("colorscheme rose-pine") end, lazy = false }
 -- return { 'ribru17/bamboo.nvim', config = function() require('bamboo').setup({}) require('bamboo').load() end, lazy = false }
--- return { "ellisonleao/gruvbox.nvim", config = function() vim.cmd([[colorscheme gruvbox]]) end, opts = {}, lazy = false }
--- return { "Mofiqul/dracula.nvim", config = function() vim.cmd([[colorscheme dracula]]) end, opts = {}, lazy = false }
-return { "rose-pine/neovim", name = "rose-pine", config = function() vim.cmd("colorscheme rose-pine") end, lazy = false }
+-- return { "ellisonleao/gruvbox.nvim", config = function() vim.cmd("colorscheme gruvbox") end, opts = {}, lazy = false }
+-- return { "catppuccin/nvim", name = "catppuccin", config = function() vim.cmd("colorscheme catppuccin") end, lazy = false }
 -- return {}
