a6d56650d2a8b2c1ac6855e2c44684288adad3a9
Jack Gitter <jack.gitter@gmail.com>
2025-02-15 13:33:37 -0500

    nice

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -7,8 +7,10 @@ return {
 		local luasnip = require("luasnip")
 		vim.keymap.set({ "i", "s" }, "<C-l>", function() luasnip.jump(1) end, { silent = true })
 		vim.keymap.set({ "i", "s" }, "<C-h>", function() luasnip.jump(-1) end, { silent = true })
+		require("luasnip").config.setup({ store_selection_keys = "<Tab>" })
 		require("luasnip.loaders.from_vscode").lazy_load()
 		require("luasnip.loaders.from_snipmate").lazy_load()
+		require("luasnip.loaders.from_lua").load({ paths = "./snippets" })
 	end,
 	event = { "BufReadPost", "BufNewFile" },
 }
