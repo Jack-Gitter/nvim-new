d61712199ca8754a68251dae46b36fca1859e68e
Jack Gitter <jack.gitter@gmail.com>
2025-02-17 13:14:52 -0500

    nice

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -10,16 +10,28 @@ return {
 	},
 	config = function(_, opts)
 		local fzf = require("fzf-lua")
+
 		fzf.setup(opts)
 		fzf.register_ui_select()
-		vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>")
-		vim.keymap.set("n", "<leader>bu", "<cmd>FzfLua buffers<cr>")
-		vim.keymap.set("n", "<leader>gr", "<cmd>FzfLua live_grep<cr>")
-		vim.keymap.set("n", "<leader>gd", "<cmd>FzfLua lsp_definitions<cr>")
-		vim.keymap.set("n", "<leader>fr", "<cmd>FzfLua lsp_references<cr>")
-		vim.keymap.set("n", "<leader>gb", "<cmd>FzfLua git_branches<cr>")
-		vim.keymap.set("n", "<leader>fd", "<cmd>FzfLua lsp_document_diagnostics<cr>")
-		vim.keymap.set("n", "<leader>ca", "<cmd>FzfLua lsp_code_actions<cr>")
+
+		local ff = function()
+			local folders = vim.lsp.buf.list_workspace_folders()
+			if next(folders) ~= nil then
+				fzf.files({ cwd = folders[1] })
+			else
+				fzf.files()
+			end
+		end
+
+		vim.keymap.set("n", "<leader>ff", ff)
+		vim.keymap.set("n", "<leader>bu", fzf.buffers)
+		vim.keymap.set("n", "<leader>gr", fzf.live_grep)
+		vim.keymap.set("n", "<leader>gb", fzf.git_branches)
+		vim.keymap.set("n", "<leader>ws", fzf.lsp_workspace_symbols)
+		vim.keymap.set("n", "<leader>fd", fzf.lsp_document_diagnostics)
+		vim.keymap.set("n", "<leader>gd", fzf.lsp_definitions)
+		vim.keymap.set("n", "<leader>ca", fzf.lsp_code_actions)
+		vim.keymap.set("n", "<leader>fr", fzf.lsp_references)
 	end,
 	keys = { "<leader>ff", "<leader>b", "<leader>gr", "<leader>gd", "<leader>fr" },
 }
