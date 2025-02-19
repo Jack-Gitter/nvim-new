63ef3008dc751deebdb853ec583d1bff1feb53ea
Jack Gitter <jack.gitter@sentinelgroup.com>
2025-02-19 14:04:51 -0500

    nice

--- lua/config/general/keymaps.lua
+++ lua/config/general/keymaps.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<space>", "<nop>", { silent = true })
 vim.keymap.set("i", "jk", "<esc>")
-vim.keymap.set("n", "<c-l>", "<c-o>")
-vim.keymap.set("n", "<c-n>", "<c-i>")
+vim.keymap.set("n", "<c-l>", "<c-^>")
