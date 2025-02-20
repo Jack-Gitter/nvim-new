2b66ead7b93d2b6291cc56109b3f65d3d6a6687a
Jack Gitter <jack.gitter@gmail.com>
2025-02-20 17:44:04 -0500

    ok js working

--- lua/plugins/nvimdap.lua
+++ lua/plugins/nvimdap.lua
@@ -8,7 +8,7 @@ return {
             port = "${port}",
             executable = {
                 command = "node",
-                args = { vim.fn.stdpath("data") .. "hello world", "${port}" },
+                args = { vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js", "${port}" },
             }
         }
         dap.configurations.javascript = {
@@ -19,6 +19,13 @@ return {
                 program = "${file}",
                 cwd = "${workspaceFolder}",
             },
+            {
+                type = "pwa-node",
+                request = "attach",
+                name = "Attach",
+                processId = require 'dap.utils'.pick_process,
+                cwd = "${workspaceFolder}",
+            }
         }
     end
 }
